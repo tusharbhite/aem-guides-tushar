b00c480d71b8b6e937fc044f130950ba00b01a42
tusharbhite <tushbhite@gmail.com>
2024-02-23 23:49:35 +0530

    icon pickers

---
+++ ui.apps/src/main/content/jcr_root/apps/tushar/components/01_tushar_components/use-api-sample/logic-with-parameter.js
@@ -0,0 +1,11 @@
+/* logic.js file: */
+use(function () {
+    var myData = {
+        str: "foo",
+        arr: [1, 2, 3]
+    };
+
+    return {
+        json: JSON.stringify(myData)
+    };
+});
---
+++ ui.apps/src/main/content/jcr_root/apps/tushar/components/01_tushar_components/use-api-sample/logic.js
@@ -0,0 +1,7 @@
+use(function () {
+    return {
+        link:  "#my link's safe",
+        title: "my title's safe",
+        text:  "my text's safe"
+    };
+});
